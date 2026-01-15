ca471184dafefc9ecedc40b410755ffc30634578
Alex Quennelle <BluePhoenixCG@gmail.com>
2026-01-15 17:49:17 -0600

    Clean up main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,7 @@
 #include "program.h"
 
-#include <print>
-#ifdef __EMSCRIPTEN__
-#include<emscripten/emscripten.h>
-#endif // __EMSCRIPTEN__
-
 auto main() -> int
 {
-	std::println("Hello, World!");
 	Program game;
 	game.Run();
 }
